1fe901dfd808d6771a4784047860bca8262f0b6f
Francisco-G-P <francisco.gonzalez@encora.com>
2024-11-10 02:43:44 -0700

    Update 46.Permutations.py

--- 46.Permutations.py
+++ 46.Permutations.py
@@ -6,19 +6,22 @@ class Solution(object):
         """
         result = []
 
-        # Base case.
+        # Base case (return single item in list).
         if len(nums) == 1:
             return [nums[:]]
-
+            
+        # For as the length of the original list the first item will be popped and permutations will be made with the remainig items.
         for _ in range(len(nums)):
             n = nums.pop(0)
             perms = self.permute(nums)
 
+            # The item popped is now appended to the smaller total permutations.
             for perm in perms:
                 perm.append(n)
 
+            # Extend new permutations and append the item that was popped.
             result.extend(perms)
             nums.append(n)
 
         return result
-        
+    
