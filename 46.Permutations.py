8525a417b99dcfde17dff4b30cfe56c4965d74a1
Francisco G P <francisco.gonzalez@encora.com>
2024-11-09 20:51:21 -0700

    46.Permutations.py Commit

---
+++ 46.Permutations.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result  = []
+
+        # Base case.
+        if len(nums) == 1:
+            return [nums[:]]
+
+        for _ in range(len(nums)):
+            n = nums.pop(0)
+            perms = self.permute(nums)
+
+            for perm in perms:
+                perm.append(n)
+
+            result.extend(perms)
+            nums.append(n)
+
+        return result
+        
